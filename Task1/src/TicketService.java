1870f6163ddb76418ce85562b8b5097dc8d6fefc
Olga Kuzmina <o.l.kuzmins@gmail.com>
2024-10-03 16:55:39 +0200

    Task JFB-2 with teammate

--- Task1/src/TicketService.java
+++ Task1/src/TicketService.java
@@ -1,13 +1,38 @@
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class TicketService {
 
     public static void main(String[] args) {
+        ArrayList<Ticket> tickets = new ArrayList<>();
 
         BigDecimal price = new BigDecimal("60.55");
-        Ticket Ticket1 = new Ticket();
-        Ticket Ticket2 = new Ticket("123A", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.A, (float) 5.001, price);
-        Ticket Ticket3 = new Ticket("Theatre", (short) 124, new Timestamp(System.currentTimeMillis()));
+        tickets.add(new Ticket("111A", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.A, (float) 5.00, price));
+        tickets.add(new Ticket("112A", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.A, (float) 5.00, price));
+        tickets.add(new Ticket("113A", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.A, (float) 5.00, price));
+        tickets.add(new Ticket("120B", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.B, (float) 5.00, price));
+        tickets.add(new Ticket("121B", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.B, (float) 5.00, price));
+        tickets.add(new Ticket("122B", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.B, (float) 5.00, price));
+        tickets.add(new Ticket("131C", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.C, (float) 5.00, price));
+        tickets.add(new Ticket("132C", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.C, (float) 5.00, price));
+        tickets.add(new Ticket("133C", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.C, (float) 5.00, price));
+        tickets.add(new Ticket("134C", "Opera", (short) 123, new Timestamp(System.currentTimeMillis()), false, StadiumSector.C, (float) 5.00, price));
+
+
+        ArrayList<Ticket> sectorTickets = findByStadiumSector(tickets, StadiumSector.A);
+        for (Ticket ticket : sectorTickets) {
+            System.out.println(ticket);
+        }
+    }
+
+    public static ArrayList<Ticket> findByStadiumSector(ArrayList<Ticket> tickets, StadiumSector sector) {
+        ArrayList<Ticket> sectorTickets = new ArrayList<>();
+        for (Ticket ticket : tickets) {
+            if (ticket.getStadiumSector().equals(sector)) {
+                sectorTickets.add(ticket);
+            }
+        }
+        return sectorTickets;
     }
 }
